581196ced23c4387fc11648523c20a0d4c823c3e
SOLIYANA KIBROM MEHIBUB <soliyanakb1@gmail.com>
2026-04-20 21:11:45 +0300

    Add user logout and CSRF protection functions

--- auth.php
+++ auth.php
@@ -30,3 +30,39 @@ function login_user($user_id)
     $_SESSION['user_id'] = $user_id;
 }
 
+function logout_user()
+{
+    // Clear session and cookie
+    $_SESSION = [];
+    if (ini_get('session.use_cookies')) {
+        $params = session_get_cookie_params();
+        setcookie(session_name(), '', time() - 42000,
+            $params['path'], $params['domain'], $params['secure'], $params['httponly']
+        );
+    }
+    session_destroy();
+}
+
+function require_login()
+{
+    if (!is_logged_in()) {
+        header('Location: /afro/login.php');
+        exit;
+    }
+}
+
+// CSRF helpers
+function csrf_token()
+{
+    if (empty($_SESSION['csrf_token'])) {
+        $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+    }
+    return $_SESSION['csrf_token'];
+}
+
+function verify_csrf($token)
+{
+    return !empty($token) && !empty($_SESSION['csrf_token']) && hash_equals($_SESSION['csrf_token'], $token);
+}
+
+?>
