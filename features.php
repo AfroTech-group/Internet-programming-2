d5ac1ff6e91974e08f90ec813e3d5701c5f50e8b
YEABSIRA BELAY ASFAW <yeabsira.belay@aastustudent.edu.et>
2026-04-20 23:46:07 +0300

    Refactor HTML processing in features.php

--- features.php
+++ features.php
@@ -12,22 +12,16 @@ $map = [
 ];
 $html = str_replace(array_keys($map), array_values($map), $html);
 
-// Add theme.css if not present
 if (strpos($html, 'theme.css') === false) {
 	$html = str_replace('</head>', '<link rel="stylesheet" href="/afro/theme.css"></head>', $html);
 }
-
-// Strip old static nav
 $html = preg_replace('/<nav class="navbar"[^>]*>.*?<\/nav>/s', '', $html);
 
-// Strip old static footer
 $html = preg_replace('/<footer class="footer"[^>]*>.*?<\/footer>/s', '', $html);
 
-// Inject PHP header after <body>
 ob_start(); include __DIR__ . '/includes/header.php'; $headerHtml = ob_get_clean();
 $html = preg_replace('/(<body[^>]*>)/i', "$1\n" . $headerHtml, $html, 1);
 
-// Inject PHP footer before </body>
 ob_start(); include __DIR__ . '/includes/footer.php'; $footerHtml = ob_get_clean();
 $html = str_replace('</body>', $footerHtml . '</body>', $html);
 
