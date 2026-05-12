9f6c071a19cc73d08f5492178803d43596b48198
Yeabsira_Tarekegn <yeabsira.tarekegn@aastustudent.edu.et>
2026-05-12 10:45:24 +0300

    Fix booking error message in event_detail.php

--- event_detail.php
+++ event_detail.php
@@ -111,7 +111,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['action']) && $_POST['
         }
     }
 }
-
+// the html part of event_detail
 ?><!doctype html>
 <html lang="en">
 <head>
